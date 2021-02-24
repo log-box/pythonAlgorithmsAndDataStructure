e99d8a9c74eef7598905612f58b49625592357f8
artem <a.y.logvin@gmail.com>
2021-02-24 09:57:28 +0300

    Добавил файлы

--- les1_task_4.py
+++ les1_task_4.py
@@ -5,18 +5,4 @@
 
 from random import random
 
-m1 = int(input())
-m2 = int(input())
-n = int(random() * (m2 - m1 + 1)) + m1
-print(n)
-
-m1 = float(input())
-m2 = float(input())
-n = random() * (m2 - m1) + m1
-print(round(n, 3))
-
-m1 = ord(input())
-m2 = ord(input())
-n = int(random() * (m2 - m1 + 1)) + m1
-print(chr(n))
 
