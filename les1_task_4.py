9fa2960a26e602c5a03080ab1357b03d66eb81a6
artem <a.y.logvin@gmail.com>
2021-02-24 09:56:00 +0300

    добавил файлы блок-схем

--- les1_task_4.py
+++ les1_task_4.py
@@ -3,3 +3,20 @@
 # границы диапазона. Например, если надо получить случайный символ от 'a' до 'f', то вводятся эти символы.
 # Программа должна вывести на экран любой символ алфавита от 'a' до 'f' включительно.
 
+from random import random
+
+m1 = int(input())
+m2 = int(input())
+n = int(random() * (m2 - m1 + 1)) + m1
+print(n)
+
+m1 = float(input())
+m2 = float(input())
+n = random() * (m2 - m1) + m1
+print(round(n, 3))
+
+m1 = ord(input())
+m2 = ord(input())
+n = int(random() * (m2 - m1 + 1)) + m1
+print(chr(n))
+
