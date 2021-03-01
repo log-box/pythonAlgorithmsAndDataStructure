13c48190c087b1ca24c718028945889753c9cf55
artem <a.y.logvin@gmail.com>
2021-03-01 19:57:32 +0300

    написал код 8 задания. Схему сразу пока не могу... рисую схему

--- les2_task_8.py
+++ les2_task_8.py
@@ -2,3 +2,24 @@
 # и цифра, которую необходимо посчитать, задаются вводом с клавиатуры.
 
 
+int_count = int(input('Сколько чисел будете вводить?\n'))
+digit = int(input('Какую цифру надо подсчитать?\n'))
+count = 0
+
+
+def recurs_func(sequence, dig, count=0):
+    if sequence == 0:
+        return count
+    if sequence % 10 == dig:
+        count += 1
+    return recurs_func(sequence // 10, dig, count)
+
+
+for i in range(1, int_count + 1):
+    number = int(input('Число ' + str(i) + ': '))
+    count = count + recurs_func(number, digit)
+print(f'Искомая цифра встретилась {count} раз!')
+
+
+
+
