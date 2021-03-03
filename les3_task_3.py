0517352df4a148eef961f69bc8c4baec0b94058b
artem <a.y.logvin@gmail.com>
2021-03-03 20:27:47 +0300

    сделал третье задание

--- les3_task_3.py
+++ les3_task_3.py
@@ -3,12 +3,27 @@
 
 import random
 
-SIZE = 10
+SIZE = 45
 MIN_ITEM = 0
 MAX_ITEM = 100
 array = [random.randint(MIN_ITEM, MAX_ITEM) for _ in range(SIZE)]
 print(array)
 
-min_item = 0
-max_item = 0
+min_item = array[0]
+max_item = array[0]
+
+for i in range(len(array)):
+    if array[i] < min_item:
+        min_item = array[i]
+    elif array[i] > max_item:
+        max_item = array[i]
+print(min_item, max_item)
+
+max = array.index(max_item)
+min = array.index(min_item)
+
+array[max] = min_item
+array[min] = max_item
+
+print(array)
 
