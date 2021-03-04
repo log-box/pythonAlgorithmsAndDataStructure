19730727591bf4c388b9ae941691b3d2efa47b65
artem <a.y.logvin@gmail.com>
2021-03-04 18:17:52 +0300

    Сделал домашку

--- les3_task_4.py
+++ les3_task_4.py
@@ -11,15 +11,13 @@ count = {}
 max = 0
 
 for i in range(len(array)): # перебираем массив, записываем цифры строкой, и увеличиваем счетчик на 1
+    temp = str(array[i]) # временная переменная, хранит текущий ключ-строку
     if str(array[i]) in count.keys():  #проверяем есть ли число в словаре, если есть, то его значение увеличиваем на 1
         count[str(array[i])] += 1
     else:                               #если число не нашлось, то присваиваем его значению 1 - оно нашлось первый раз
         count[str(array[i])] = 1
-
-for value in count.values():  #находим максимальное значение в словаре
-    temp = value
-    if value >= max:
-        max = temp
+    if count[temp] >= max:  # вычисляем максимальное значение
+        max = count[temp]
 
 result_dict = {key: value for key, value in count.items() if value == max}  #генерируем новый словарь с макс. знач.
 
--- les3_task_6.py
+++ les3_task_6.py
@@ -1,3 +1,31 @@
 # В одномерном массиве найти сумму элементов, находящихся между минимальным и максимальным элементами.
 # Сами минимальный и максимальный элементы в сумму не включать.
 
+import random
+
+SIZE = 10
+MIN_ITEM = 0
+MAX_ITEM = 50
+array = [random.randint(MIN_ITEM, MAX_ITEM) for _ in range(SIZE)]
+min = MAX_ITEM
+max = 0
+sum = 0
+min_index = 0  # без них Пичарм подсвечивает в коде - неудобно
+max_index = 0  # ---
+
+for i in range(len(array)):
+    if array[i] > max:
+        max = array[i]
+        max_index = i
+    if array[i] < min:
+        min = array[i]
+        min_index = i
+
+if min_index < max_index:  # если минимальный стоит слева от максимального
+    for i in range(min_index + 1, max_index):
+        sum = sum + array[i]
+for i in range(max_index + 1, min_index): # если максимальный стоит слева от минимального
+    sum = sum + array[i]
+print(f'Ваш массив: {array}')
+print(f'Сумма между минимальным значением: {array[min_index]} и '
+      f'максимальным значением: {array[max_index]} равна: {sum}')
