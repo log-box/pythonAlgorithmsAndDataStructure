063a4932ac2107f61216338eb8fd688d879a21c6
artem <a.y.logvin@gmail.com>
2021-03-11 15:46:45 +0300

    первый вариант второй задачи (только сложение)

---
+++ les5_task_2.py
@@ -0,0 +1,88 @@
+# Написать программу сложения и умножения двух положительных целых шестнадцатеричных чисел.
+# При этом каждое число представляется как коллекция, элементы которой — цифры числа.
+# Например, пользователь ввёл A2 и C4F. Нужно сохранить их как [‘A’, ‘2’] и [‘C’, ‘4’, ‘F’] соответственно.
+# Сумма чисел из примера: [‘C’, ‘F’, ‘1’], произведение - [‘7’, ‘C’, ‘9’, ‘F’, ‘E’].
+
+
+from collections import deque
+
+
+reference_list = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F']
+x = 1
+result = deque()
+one = 0
+
+while x == 1:
+    x_number = deque(input('Введите первое число').upper())
+    for _ in x_number:
+        if _ not in reference_list:
+            print('Вы ввели не шестнадцатиричное число')
+            break
+    else:
+        x = 0
+while x == 0:
+    y_number = deque(input('Введите второе число').upper())
+    for _ in y_number:
+        if _ not in reference_list:
+            print('Вы ввели не шестнадцатиричное число')
+            break
+    else:
+        x = 1
+print(x_number, y_number)
+
+if len(x_number) > len(y_number):
+    while len(y_number) > 0:
+        temp1 = y_number.pop()
+        temp2 = x_number.pop()
+        temp1 = reference_list.index(temp1)
+        temp2 = reference_list.index(temp2)
+        if temp1 + temp2 > 15:
+            addition = abs((temp1+temp2) - 16) + one
+            one = 1
+        else:
+            addition = temp1 + temp2
+        addition = reference_list[addition]
+        result.appendleft(addition)
+    while len(x_number) > 0:
+        temp1 = x_number.pop()
+        temp1 = reference_list.index(temp1)
+        if temp1 + one > 15:
+            addition = abs((temp1+one) - 16)
+            one = 1
+        else:
+            addition = temp1+one
+        addition = reference_list[addition]
+        result.appendleft(addition)
+    if one == 1:
+        result.appendleft(str(one))
+        print(result)
+    else:
+        print(result)
+else:
+    while len(x_number) > 0:
+        temp1 = x_number.pop()
+        temp2 = y_number.pop()
+        temp1 = reference_list.index(temp1)
+        temp2 = reference_list.index(temp2)
+        if temp1 + temp2 > 15:
+            addition = abs((temp1+temp2) - 16) + one
+            one = 1
+        else:
+            addition = temp1 + temp2
+        addition = reference_list[addition]
+        result.appendleft(addition)
+    while len(y_number) > 0:
+        temp1 = y_number.pop()
+        temp1 = reference_list.index(temp1)
+        if temp1 + one > 15:
+            addition = abs((temp1+one) - 16)
+            one = 1
+        else:
+            addition = temp1+one
+        addition = reference_list[addition]
+        result.appendleft(addition)
+    if one == 1:
+        result.appendleft(str(one))
+        print(f'Сумма чисел равна: {str(result)}')
+    else:
+        print(f'Сумма чисел равна: {str(result)}')
